75ccf269ef2f21d2136bd0903867d3582ba6c61b
Alexander Kuznecov <kuznecovav92@yandex.ru>
2023-04-08 18:56:07 +0600

    - добавил инициализацию uart для работы по 1 Wire

--- hal/onewire_emul.cpp
+++ hal/onewire_emul.cpp
@@ -1,2 +1,64 @@
 #include "onewire_emul.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
+	#include "sl_assert.h"
+    #include "em_cmu.h"
+#ifdef __cplusplus
+}
+#endif
 
+OneWireEmul::OneWireEmul(const IRQn irqNumber) : _irqNumber(irqNumber) {
+    
+}
+
+OneWireEmul::Result OneWireEmul::Init(config_t &config) {
+    CMU_Clock_TypeDef clock;
+    if (config.port == USART0) {
+        clock = cmuClock_USART0;
+    } else if (config.port == USART1) {
+        clock = cmuClock_USART1;
+    } else if (config.port == USART2) {
+        clock = cmuClock_USART2;
+    } else {
+        return Result::ErrorPort;
+    }
+    CMU_ClockEnable(clock, true);
+    CMU_ClockEnable(cmuClock_GPIO, true);
+
+    GPIO->USARTROUTE[config.uartNum].ROUTEEN = GPIO_USART_ROUTEEN_TXPEN | GPIO_USART_ROUTEEN_RXPEN;
+    GPIO->USARTROUTE[config.uartNum].TXROUTE = (config.txPort << _GPIO_USART_TXROUTE_PORT_SHIFT) |
+                                               (config.txPin << _GPIO_USART_TXROUTE_PIN_SHIFT);
+    GPIO->USARTROUTE[config.uartNum].RXROUTE = (config.rxPort << _GPIO_USART_RXROUTE_PORT_SHIFT) |
+                                               (config.rxPin << _GPIO_USART_RXROUTE_PIN_SHIFT);
+
+    GPIO_PinModeSet(config.txPort, config.txPin, gpioModePushPull, 1);
+    GPIO_PinModeSet(config.rxPort, config.rxPin, gpioModeInputPull, 1);
+
+    /* Initialize USART registers to hardware reset state. */
+    USART_Reset(config.port);
+
+    config.port->EN_SET = USART_EN_EN;
+
+    /* Configure databits, stopbits, and parity. */
+    config.port->FRAME = (uint32_t)usartDatabits8 | (uint32_t)usartStopbits1 | (uint32_t)usartNoParity;
+
+    /* Configure baudrate. */
+    USART_BaudrateAsyncSet(config.port, 0, 9600, usartOVS16);
+
+    // #if defined(_USART_TIMING_CSHOLD_MASK)
+    //     usart->TIMING = (((uint32_t)init->autoCsHold << _USART_TIMING_CSHOLD_SHIFT) & _USART_TIMING_CSHOLD_MASK) |
+    //                     (((uint32_t)init->autoCsSetup << _USART_TIMING_CSSETUP_SHIFT) & _USART_TIMING_CSSETUP_MASK);
+    // #endif
+
+    GPIO->USARTROUTE_CLR[USART_NUM(config.port)].ROUTEEN = GPIO_USART_ROUTEEN_RTSPEN;
+    config.port->CTRLX_CLR = USART_CTRLX_CTSEN;
+
+    config.port->CTRL |= USART_CTRL_LOOPBK_ENABLE | USART_CTRL_AUTOTRI_ENABLE;
+
+    /* Finally, enable (as specified). */
+    config.port->CMD = USART_CMD_CLEARRX | USART_CMD_CLEARTX;
+
+    // Clear any false IRQ/DMA request
+    USART_IntClear(config.port, ~0x0);
+}
--- hal/onewire_emul.h
+++ hal/onewire_emul.h
@@ -1,14 +1,35 @@
 #pragma once
 
-#include "uart.h"
+#include <stddef.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
+	#include "em_usart.h"
+    #include "em_gpio.h"
+#ifdef __cplusplus
+}
+#endif
+#include "interfaces/i_data_exchange.h"
 
-class OneWireEmul : public Uart {
-    public :
-        OneWireEmul(const IRQn irqNumber,
-                  typeTransmiteCallback transmiteCallback) : Uart(irqNumber,
-                                                                  transmiteCallback) {
+class OneWireEmul : public IDataExchange {
+public:
+    enum class Result : uint8_t {
+        Success,
+        ErrorPort,
+        ErrorFatal,
+    };
+    typedef struct {
+        USART_TypeDef *port;  /* The peripheral used for UART */
+        uint8_t uartNum;      /* UART instance number */
+        GPIO_Port_TypeDef txPort; /* Port for UART Tx pin */
+        GPIO_Port_TypeDef rxPort; /* Port for UART Rx pin */
+        uint8_t txPin;            /* Pin number for UART Tx */
+        uint8_t rxPin;            /* Pin number for UART Rx */
 
-        }
-private :
+    } config_t;
+    OneWireEmul(const IRQn irqNumber);
+    Result Init(config_t &config);
 
+private:
+    const IRQn _irqNumber;
 };
